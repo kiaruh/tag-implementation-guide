62c95ba76ec840bb80eb9516c574c2d116e78b9e
kiaruh <nikoz.li@gmail.com>
2024-03-26 02:29:45 -0300

    template finished

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
-const express = require('express');
-const fs =require('fs')
+const express = require("express");
+const fs = require("fs");
 const app = express();
-const path = require('path');
-const { send } = require('process');
+const path = require("path");
+const { send } = require("process");
 
-app.use(express.static(path.resolve(__dirname,'./public')));
+app.use(express.static(path.resolve(__dirname, "./public")));
 
-app.set('port',process.env.PORT || 3000)
+app.set("port", process.env.PORT || 3000);
 
-app.listen(app.get('port'),() => console.log('Server start http://localhost:' +app.get('port')));
+app.listen(app.get("port"), () =>
+  console.log("Server start http://localhost:" + app.get("port"))
+);
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./views','home.html'))
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views", "home.html"));
+});
 
-})
+app.get("/checkout", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views", "checkout.html"));
+});
 
-app.get('/info',(req,res)=> {
-    res.sendFile(path.resolve(__dirname,'./views','info.html'))
-})
+app.get("/shop", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views", "shop.html"));
+});
+
+app.get("/about-us", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views", "about-us.html"));
+});
+
+app.get("/contact-us", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views", "contact-us.html"));
+});
+
+app.get("/confirmation", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views", "confirmation.html"));
+});
+app.get("/survey", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views", "survey.html"));
+});
+app.get("/info", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views", "info.html"));
+});
+
+app.get("/confirmation", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views", "confirmation.html"));
+});
